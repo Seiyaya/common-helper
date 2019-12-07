cff1e6eb5447b588ee45b26386bbafe44086ab50
wangjia <w806179263@qq.com>
2019-12-07 13:19:00 +0800

    spring代码相关测试

---
+++ common-tester/src/main/java/xyz/seiyaya/boot/controller/BookController.java
@@ -0,0 +1,11 @@
+package xyz.seiyaya.boot.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author seiyaya
+ * @date 2019/11/3 23:48
+ */
+@Controller
+public class BookController {
+}
---
+++ common-tester/src/main/java/xyz/seiyaya/boot/service/BookService.java
@@ -0,0 +1,11 @@
+package xyz.seiyaya.boot.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author seiyaya
+ * @date 2019/11/3 23:48
+ */
+@Service
+public class BookService {
+}
