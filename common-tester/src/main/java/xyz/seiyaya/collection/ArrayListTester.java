cb1bdfb579df08b0ebfc37b14a1228323478ee62
wangjia <w806179263@qq.com>
2020-01-16 17:35:52 +0800

    测试并发环境下添加元素

--- common-tester/src/main/java/xyz/seiyaya/collection/ArrayListTester.java
+++ common-tester/src/main/java/xyz/seiyaya/collection/ArrayListTester.java
@@ -1,6 +1,12 @@
 package xyz.seiyaya.collection;
 
+import org.junit.Test;
+
 import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * ArrayList实现
@@ -8,14 +14,38 @@ import java.util.ArrayList;
  * @version 1.0
  * @date 2019/11/4 17:29
  */
+@SuppressWarnings("all")
 public class ArrayListTester {
 
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(1);
-        System.out.println(list);
+        String one = "1";
+        Boolean b1 = Boolean.parseBoolean(one); // line n1
+        Integer i1 = new Integer(one);
+        Integer i2 = 1;
+        Integer i3 = 2-1;
+        System.out.println(i1 == i2);
+        System.out.println(i2 == i3);
+        System.out.println(i3 == i1);
+    }
 
-        int result = 10 + (10 >> 1);
-        System.out.println(String.format("result:%s",result));
+    @Test
+    public void testConcurrencyAddElement() throws InterruptedException {
+        // 添加元素引起的并发问题
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(10,20,1, TimeUnit.SECONDS,new LinkedBlockingQueue<>());
+        List<Integer> list = new ArrayList<>();
+        for(int i=0;i<100;i++){
+            int finalI = i;
+            executor.execute(() -> {
+                list.add(finalI);
+            });
+        }
+
+        TimeUnit.SECONDS.sleep(2);
+
+        for(int i=0;i<100;i++){
+            System.out.println(i + ":--> " + list.get(i));
+        }
+
+        System.out.println("list size:"+list.size());
     }
 }
--- common-tester/src/main/java/xyz/seiyaya/thread/README.md
+++ common-tester/src/main/java/xyz/seiyaya/thread/README.md
@@ -1,6 +1,1 @@
-# 问题
-+ wait,sleep分别是谁的方法，区别
-+ countLatch的await方法是否安全，怎么改造
-+ 线程池参数，整个流程描述
-+ 背后的底层原理aqs，cas
-+ ThreadLocal原理，注意事项，参数传递
+# 《Java并发编程实战》
